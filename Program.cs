da7994c399f6dea5a51d97fed596c228a1488597
Felipe Amelong <felipeamelong@gmail.com>
2026-08-10 10:27:44 -0300

    etapa 3: carga multiple con menu

--- Program.cs
+++ Program.cs
@@ -5,11 +5,39 @@ Console.WriteLine();
 Console.WriteLine($"{NombreKiosco}");
 Console.WriteLine($"Bienvenido, {NombreCajero}. Caja abierta");
 Console.WriteLine();
-Console.Write("Ingrese el nombre de un producto: ");
-string NombreProducto = Console.ReadLine();
-Console.Write("Ingrese el precio del producto: ");
-decimal PrecioProducto = decimal.Parse(Console.ReadLine());
-Console.WriteLine();
-Console.WriteLine($"El producto {NombreProducto} tiene un precio de ${PrecioProducto}");
-Console.WriteLine();
+int CantidadProductos = 0;
+int decision = 1;
+do
+{
+
+    Console.WriteLine("¿Qué desea hacer?\n1 - Cargar un producto\n2 - Cerrar la venta");
+    decision = int.Parse(Console.ReadLine());
+
+    switch (decision)
+    {
+        case 1:
+        {
+            CantidadProductos++;
+            Console.Write("Ingrese el nombre de un producto: ");
+            string NombreProducto = Console.ReadLine();
+            Console.Write("Ingrese el precio del producto: ");
+            decimal PrecioProducto = decimal.Parse(Console.ReadLine());
+            Console.WriteLine();
+            Console.WriteLine($"El producto {NombreProducto} tiene un precio de ${PrecioProducto}");
+            Console.WriteLine();
+            break;
+        }
+        case 2:
+        {
+            Console.Write($"Trabajo finalizado por hoy. Se cargaron {CantidadProductos} productos.");
+            break;
+        }
+        default:
+        {
+            Console.WriteLine("Opción no válida");
+            break;
+        }
+    }
+} while (decision != 2);
+
 Console.ReadKey();
