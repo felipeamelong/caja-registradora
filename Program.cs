f8e13a7f6d6b2ad8167163aa2fd84305c49894bd
Felipe Amelong <felipeamelong@gmail.com>
2026-08-10 09:51:19 -0300

    etapa 1: bienvenida y datos del cajero

--- Program.cs
+++ Program.cs
@@ -1,1 +1,7 @@
-﻿Console.WriteLine("Hello, World!");
+﻿string NombreKiosco = "Kiosco Siempre";
+Console.Write("Ingrese el nombre del cajero: ");
+string NombreCajero = Console.ReadLine();
+Console.WriteLine();
+Console.WriteLine($"{NombreKiosco}");
+Console.WriteLine($"Bienvenido, {NombreCajero}. Caja abierta");
+Console.ReadKey();
